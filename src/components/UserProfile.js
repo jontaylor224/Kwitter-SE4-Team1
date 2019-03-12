71138535eaa99f00288d63e01621bbc30b7c8a5a
Jon Taylor <jontaylor224@gmail.com>
2019-03-12 08:09:47 -0500

    started user profile component

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,39 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
+import logo from "../logo.svg"
+import { Card, Divider, Image, Header, Segment, Grid } from "semantic-ui-react"
 
 class UserProfile extends Component {
-  render() {
-    return <p>This is the user profile</p>;
-  }
+    render() {
+        return (
+            <Grid>
+                <Grid.row>
+                    <Grid.column width={6}>
+                        <Card>
+                            <Card.content>
+                                <Image src={logo} />
+                                <Card.Header as="h2" textAlign="center">
+                                    {this.props.username}
+                                </Card.Header>
+                                <Divider />
+                                <Card.Description>
+                                    <Card.Meta as="h3">Bio:</Card.Meta>
+                                    {this.props.about}
+                                </Card.Description>
+                            </Card.content>
+                        </Card>
+                    </Grid.column>
+                    <Grid.column floating="right" width={10}>
+                        <Segment>
+                            <Header as="H2" textAlign="center">
+                                My Messages
+                            </Header>
+                            {/* <UserMessages /> */}
+                        </Segment>
+                    </Grid.column>
+                </Grid.row>
+            </Grid>
+        )
+    }
 }
 
-export default UserProfile;
+export default UserProfile
