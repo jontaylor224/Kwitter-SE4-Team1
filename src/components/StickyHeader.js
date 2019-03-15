8988b69e2ab167bc144df1edbe0d261ae46e2559
Peter M <petem14@gmail.com>
2019-03-15 01:18:59 -0400

    conditional rendering header

--- src/components/StickyHeader.js
+++ src/components/StickyHeader.js
@@ -1,46 +1,48 @@
-import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
-import 'semantic-ui-css/semantic.min.css';
-import ImageExampleAvatar from "./Avatar"
-import { Link } from "react-router-dom"
+import React, { Component } from "react";
+import { Menu } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import ImageExampleAvatar from "./Avatar";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
-
 class StickyHeader extends Component {
-    // handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-    render() {
-        return (
-            <Menu secondary>
-                <Menu.Item>
-                    <Link to="/home">Home</Link>
-                </Menu.Item>
-                <Menu.Item>
-                    <Link to="/">Login</Link>
-                </Menu.Item>
-                <Menu.Item>
-                    <Link to="/register">Register</Link>
-                </Menu.Item>
-                <Menu.Item>
-                    <Link to="/profile">Profile</Link>
-                </Menu.Item>
-                { this.props.login!==null ?
-                <Menu.Menu position='right'>
-                    <Menu.Item>
-                        <ImageExampleAvatar />
-                    </Menu.Item>
-                    <Menu.Item
-                        name='logout'
-                    />
-                </Menu.Menu>
-                :<div></div>}
-            </Menu>
-        )
-    }
+  // handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  render() {
+    return (
+      <Menu secondary>
+        {this.props.login !== null ? (
+          <React.Fragment>
+            <Menu.Item>
+              <Link to="/home">Home</Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to="/profile">Profile</Link>
+            </Menu.Item>
+            <Menu.Menu position="right">
+              <Menu.Item>
+                <ImageExampleAvatar />
+              </Menu.Item>
+              <Menu.Item name="logout" />
+            </Menu.Menu>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <Menu.Item>
+              <Link to="/">Login</Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to="/register">Register</Link>
+            </Menu.Item>
+          </React.Fragment>
+        )}
+      </Menu>
+    );
+  }
 }
 
 export default connect(
-    ({ auth }) => ({
-      login: auth.login
-    }),
-    null
-  )(StickyHeader);
+  ({ auth }) => ({
+    login: auth.login
+  }),
+  null
+)(StickyHeader);
