21f8dc011df690942e9ace53507c903645a93cb6
Peter M <petem14@gmail.com>
2019-03-18 09:45:55 -0400

    added avatar to homeview

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -3,6 +3,7 @@ import { Grid } from "semantic-ui-react";
 import MessageForm from "./MessageForm";
 import StickyHeader from "./StickyHeader";
 import MessageList from "./MessageList";
+import UserAvatar from "./UserAvatar"
 
 export class HomeView extends Component {
   render() {
@@ -10,7 +11,10 @@ export class HomeView extends Component {
       <React.Fragment>
         <StickyHeader />
         <Grid container stackable>
-          <Grid.Row columns={1}>
+          <Grid.Row columns={2}>
+          <Grid.Column width={5}>
+            <UserAvatar/>
+          </Grid.Column>
             <Grid.Column width={5}>
               <MessageForm />
               <MessageList />
