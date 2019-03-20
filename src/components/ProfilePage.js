0ed4c08a6664d75b5fb065c88c5867c4a47ffb99
Peter M <petem14@gmail.com>
2019-03-20 01:04:00 -0400

    format / add card / style margin auto

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,35 +1,37 @@
-import React, { Component } from "react"
-import StickyHeader from "./StickyHeader"
-import UserProfile from "./UserProfile"
-import { Grid, Segment, Header } from "semantic-ui-react"
-import UpdateProfileForm from "./UpdateProfileForm"
-import MessageList from "./MessageList"
-import UpdateProfilePicture from "./UpdateProfilePicture"
+import React, { Component } from "react";
+import StickyHeader from "./StickyHeader";
+import UserProfile from "./UserProfile";
+import { Grid, Segment, Header, Card } from "semantic-ui-react";
+import UpdateProfileForm from "./UpdateProfileForm";
+import MessageList from "./MessageList";
+import UpdateProfilePicture from "./UpdateProfilePicture";
 
 export default class ProfilePage extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <StickyHeader />
-                <Grid container stackable>
-                    <Grid.Row />
-                    <Grid.Row>
-                        <Grid.Column floated="left" width={6}>
-                            <UserProfile />
-                            <UpdateProfilePicture />
-                            <UpdateProfileForm />
-                        </Grid.Column>
-                        <Grid.Column floated="right" width={10}>
-                            <Segment>
-                                <Header as="h2" textAlign="center">
-                                    My Messages
-                                </Header>
-                                <MessageList />
-                            </Segment>{" "}
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <StickyHeader />
+        <Grid container stackable>
+          <Grid.Row />
+          <Grid.Row>
+            <Grid.Column floated="left" width={6}>
+              <UserProfile />
+              <Card style={{margin:'auto'}}>
+                <UpdateProfilePicture />
+                <UpdateProfileForm />
+              </Card>
+            </Grid.Column>
+            <Grid.Column floated="right" width={10}>
+              <Segment>
+                <Header as="h2" textAlign="center">
+                  My Messages
+                </Header>
+                <MessageList />
+              </Segment>{" "}
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </React.Fragment>
+    );
+  }
 }
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,45 +1,44 @@
-import React, { Component } from "react"
-import { Card, Divider } from "semantic-ui-react"
-import { connect } from "react-redux"
-import { getLoggedInUserInfo } from "../actions"
-import UserAvatar from "./UserAvatar"
+import React, { Component } from "react";
+import { Card, Divider } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { getLoggedInUserInfo } from "../actions";
+import UserAvatar from "./UserAvatar";
 
 class UserProfile extends Component {
-    componentDidMount() {
-        this.props.getLoggedInUserInfo()
-    }
-    render() {
-        return (
-            <Card>
-                <Card.Content>
-                    <UserAvatar size="medium" />
-                    <Divider />
-                    <Card.Meta>User since {this.props.createdAt}.</Card.Meta>
-                    <Card.Header as="h4">Bio:</Card.Header>
-                    <Card.Description>
-                        {this.props.about ||
-                            "This user has not yet created a bio"}
-                    </Card.Description>
-                </Card.Content>
-            </Card>
-        )
-    }
+  componentDidMount() {
+    this.props.getLoggedInUserInfo();
+  }
+  render() {
+    return (
+      <Card style={{margin:'auto'}}>
+        <Card.Content>
+          <UserAvatar size="medium" />
+          <Divider />
+          <Card.Meta>User since {this.props.createdAt}.</Card.Meta>
+          <Card.Header as="h4">Bio:</Card.Header>
+          <Card.Description>
+            {this.props.about || "This user has not yet created a bio"}
+          </Card.Description>
+        </Card.Content>
+      </Card>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        displayName: state.users.loggedInUser.displayName,
-        about: state.users.loggedInUser.about,
-        username: state.users.loggedInUser.username,
-        createdAt: state.users.loggedInUser.createdAt
-    }
-}
+  return {
+    displayName: state.users.loggedInUser.displayName,
+    about: state.users.loggedInUser.about,
+    username: state.users.loggedInUser.username,
+    createdAt: state.users.loggedInUser.createdAt
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-    return { getLoggedInUserInfo: () => dispatch(getLoggedInUserInfo()) }
-}
+  return { getLoggedInUserInfo: () => dispatch(getLoggedInUserInfo()) };
+};
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(UserProfile)
+  mapStateToProps,
+  mapDispatchToProps
+)(UserProfile);
