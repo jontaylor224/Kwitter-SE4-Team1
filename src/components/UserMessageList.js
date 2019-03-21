f90a6921cde1818baffc4f02631f184063ed021e
Survi <suravitaroy@gmail.com>
2019-03-21 15:36:48 -0400

    added userMessageItem component

--- src/components/UserMessageList.js
+++ src/components/UserMessageList.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import MessageItem from "./MessageItem";
+import UserMessageItem from "./UserMessageItem";
 import { connect } from "react-redux";
 
 export class UserMessageList extends Component {
   render() {
     return (
       <React.Fragment>
-        {this.props.loggedInUser.messages.map(message => (
-          <MessageItem
+        {this.props.loggedInUser.messages.reverse().map(message => (
+          <UserMessageItem
             key={message.id}
             message={message}
             displayName={this.props.loggedInUser.displayName}
