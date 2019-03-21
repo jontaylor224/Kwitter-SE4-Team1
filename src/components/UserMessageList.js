2341c6bc665d10997cc17daf146b1ef3167b056c
Peter M <petem14@gmail.com>
2019-03-21 15:45:41 -0400

    sort user messages

--- src/components/UserMessageList.js
+++ src/components/UserMessageList.js
@@ -6,7 +6,9 @@ export class UserMessageList extends Component {
   render() {
     return (
       <React.Fragment>
-        {this.props.loggedInUser.messages.reverse().map(message => (
+        {this.props.loggedInUser.messages.sort((a,b)=>{
+          return b.id - a.id
+        }).map(message => (
           <UserMessageItem
             key={message.id}
             message={message}
